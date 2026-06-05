e728e3469805d956cabbe86a626c79f4f44973d1
Joey McKenzie <joey@givebutter.com>
2026-06-05 09:30:35 -0700

    chore: idk updates

--- nix-darwin/home/shell.nix
+++ nix-darwin/home/shell.nix
@@ -50,6 +50,8 @@
       dvt = "devenv test";
 
       devenv-orphans = ''ps -axo pid,ppid,command | rg -v "claude|rg" | awk "\$2==1 && /vite|php artisan|redis-server|caddy|mailpit/ {print}"'';
+
+      caddy-reload = "sudo caddy reload --config /etc/caddy/Caddyfile --adapter caddyfile";
     };
 
     initContent = ''
