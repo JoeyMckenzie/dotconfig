27592a7c14cc3fa4bbb92a78c5f8829b5c9070cb
Joey McKenzie <joey@givebutter.com>
2026-06-09 07:53:02 -0700

    chore: stuff

--- nix-darwin/home/sops.nix
+++ nix-darwin/home/sops.nix
@@ -8,5 +8,13 @@
     secrets."intelephense-licence" = {
       path = "${config.home.homeDirectory}/intelephense/licence.txt";
     };
+
+    secrets."harlequin-config" = {
+      path = "${config.home.homeDirectory}/.harlequin.toml";
+    };
+
+    secrets."lazysql-config" = {
+      path = "${config.home.homeDirectory}/.config/lazysql/config.toml";
+    };
   };
 }
