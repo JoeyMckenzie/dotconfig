f21e7c4decfa3d2ee0e544117de24aeeb6673a40
Joey McKenzie <joey.mckenzie27@gmail.com>
2026-08-01 09:20:39 -0700

    chore: nuke zellij

--- nix-darwin/home/terminal.nix
+++ nix-darwin/home/terminal.nix
@@ -1,11 +1,5 @@
-{ pkgs, ... }:
+{ _ }:
 
 {
-  home.packages = with pkgs; [
-    zellij
-  ];
-
-  xdg.configFile."zellij/config.kdl".source = ./config/zellij.kdl;
-  xdg.configFile."zellij/layouts/worktree.kdl".source = ./config/zellij-new-worktree.kdl;
   home.file."Library/Application Support/com.mitchellh.ghostty/config".source = ./config/ghostty.conf;
 }
